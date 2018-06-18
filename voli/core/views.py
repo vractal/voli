3ddc7992567dcf693bca999f2a2b57a09b58a15f
v@fractopia.org <v@fractopia.org>
2018-06-18 16:57:09 -0300

    Corrects pagination problem with filters

--- voli/core/views.py
+++ voli/core/views.py
@@ -20,27 +20,33 @@ class Home(ListView):
     context_object_name = 'recipes'
     paginate_by = 14
 
+
+
     # filtro por tag. Recebe um nome, adiciona no contexto da sessao, mostra o formulario de acordo (tags com checkbox) e o
     # query também
 # Quero pegar uma tag por post request e retornar a pagina selecionando só os que tem a tag
+
+
     def post(self, request,*args,**kwargs):
         # Porque o dicionario do 'tag` retorna apenas o ultimo valor? o responsavel eh o MULTIVALUEDICT. Query dict
         # na documentacao ele explicita isso. PORque retornar apenas o ultimo valor??
-        filter = self.request.POST.getlist("tag","")
-        # Escrever sobre como achei que tinha que escrever essa linha de baixo
-        if filter:
-            tag = Tag.objects.get(name=filter[0])
-            recipes = [recipe for recipe in tag.recipes.all()]
-            if len(filter) > 0:
-                for t in filter:
-                    tag = Tag.objects.get(name=t)
-                    r2 = [recipe for recipe in tag.recipes.all()]
-                    recipes = list(set(recipes).intersection(r2))
-            self.object_list = recipes
+        filter = self.request.POST.getlist("tag")
+        clear = self.request.POST.get("clear")
+
+
+        if filter and not clear:
+            tags = [Tag.objects.get(name=tag) for tag in filter]
+            sets = [set(tag.recipes.all()) for tag in tags]
+            recipes_set = set.intersection(*sets)
+            self.object_list = list(recipes_set)
+            self.request.session['session_filter'] = filter
         else:
             self.object_list = Recipe.objects.all()
+            self.request.session['session_filter'] = None
+
+
         context = self.get_context_data(**kwargs)
-        context['session_filter'] = filter
+
         return render(request, 'home.html', context)
 
 
@@ -49,4 +55,17 @@ class Home(ListView):
         # Add tags to context
         context = super().get_context_data(**kwargs)
         context['tags'] = Tag.objects.all()
+
         return context
+
+    def get_queryset(self):
+        try:
+            filter = self.request.session['session_filter']
+            tags = [Tag.objects.get(name=tag) for tag in filter]
+            sets = [set(tag.recipes.all()) for tag in tags]
+            recipes_set = set.intersection(*sets)
+            queryset = list(recipes_set)
+        except:
+            queryset = super().get_queryset()
+
+        return queryset
