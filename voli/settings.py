4c65971117b3055ee7e690dd27f2ff0b8933636c
v@fractopia.org <v@fractopia.org>
2018-06-19 19:05:50 -0300

    workaround error in settings file

--- voli/settings.py
+++ voli/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
-    'coverage',
     'voli.core',
     'voli.telebot',
 ]
